f90bfe7f30c346d9554852b25517965d5f38e166
German Salinas <germansalinas.fce@gmail.com>
2023-10-03 01:09:50 -0300

    cambios de respuestas api

--- API-Ecommerce/Controllers/PruebaController.cs
+++ API-Ecommerce/Controllers/PruebaController.cs
@@ -4,8 +4,8 @@ using System.Linq;
 using System.Threading.Tasks;
 using BussinessLogic.DTO;
 using BussinessLogic.Services;
-using DataAccess.Entities;
 using Microsoft.AspNetCore.Mvc;
+using AutoWrapper.Wrappers;
 
 // For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
 
@@ -28,10 +28,24 @@ namespace API_Ecommerce.Controllers
         //Metodo para traer todas las categorias
         [HttpGet]
         [Route("/categorias")]
-        public async Task<IList<CategoriaDTO>> GetCategorias()
+        public async Task<ApiResponse> GetCategorias()
         {
-            IList<CategoriaDTO> categorias = await _service.GetAllCategorias();
-            return categorias;
+            try
+            {
+                IList<CategoriaDTO> categorias = await _service.GetAllCategorias();
+                ApiResponse response = new ApiResponse(new { data = categorias, cantidadCategorias = categorias.Count() });
+                return response;
+            }
+            catch (Exception ex)
+            {
+                while (ex.InnerException != null)
+                {
+                    ex = ex.InnerException;
+                }
+                throw new ApiException(ex);
+            }
+
+
         }
 
         // POST api/values
@@ -74,7 +88,7 @@ namespace API_Ecommerce.Controllers
             IList<CategoriaDTO> categorias = await _service.BuscarCategorias(descripcion);
             return categorias;
         }
-        
+
     }
 }
 
